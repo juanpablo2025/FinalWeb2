3c766248556bd75a90e5151eef96f52a3d3da1fe
Juan Avendano <juanpablo_1810dev@hotmail.com>
2023-06-12 22:33:38 -0500

    Se crea form registrarClientes

--- src/Paginas/Clientes.jsx
+++ src/Paginas/Clientes.jsx
@@ -1,10 +1,89 @@
+import { collection, addDoc } from "firebase/firestore";
+import { dataBase,  } from "../config/dataBase.jsx";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Menu from "../Componentes/Menu.jsx"
 import Footer from '../Componentes/Footer.jsx'
 const Clientes = () => {
+  const [nombre, setNombre] = useState("");
+  const [documento, setDocumento] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [direccion, setDireccion] = useState("");
+  const [barrio, setBarrio] = useState("");
+  const [ciudad, setCiudad] = useState("");
+  /*imagen*/
+  
+  const returnListado = useNavigate();
+  const agrearCliente = async () => {
+   
+    
+    const servicioCollection = collection(dataBase, "servicios");
+    const servicio = {
+      nombre,
+      documento,
+      correo,
+      telefono,
+      direccion,
+      barrio,
+      ciudad
+      
+    };
+    await addDoc(servicioCollection, servicio);
+    returnListado("/listar");
+  };
   return (
     <div>
       <Menu/>
-      <p>Clientes</p>
+
+      <h1>Registrar Cliente</h1>
+      <section >
+      <form className="formClientes">
+        <input
+          onChange={(e) => setNombre(e.target.value)}
+          placeholder={"Nombre "}
+          type={"text"}
+        />
+        <input
+          onChange={(e) => setDocumento(e.target.value)}
+          placeholder={"Documento "}
+          type={"text"}
+          max={'50'}
+        />
+        <input
+          onChange={(e) => setCorreo(e.target.value)}
+          placeholder={"Correo"}
+          type={"text"}
+        />
+        <input
+          onChange={(e) => setTelefono(e.target.value)}
+          placeholder={"Telefono"}
+          type={"text"}
+        />
+        <input
+          onChange={(e) => setDireccion(e.target.value)}
+          placeholder={"Direccion"}
+          type={"text"}
+        />
+        <input
+          onChange={(e) => setBarrio(e.target.value)}
+          placeholder={"Barrio"}
+          type={"text"}
+        />
+        <input
+          onChange={(e) => setCiudad(e.target.value)}
+          placeholder={"Ciudad"}
+          type={"text"}
+        />
+      
+        <input
+          onClick={agrearCliente}
+          type={"button"}
+          value={"Agregar cliente"}
+        />
+      </form>
+    </section>
       <Footer/>
       </div>
   )

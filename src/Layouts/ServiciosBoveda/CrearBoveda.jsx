19034a68e2d6a4e1e4a43f4c1f1ec4d6553ae83b
Lfgomezj <123104490+Lfgomezj@users.noreply.github.com>
2023-06-26 13:13:54 -0500

    servicios crear y listar boveda

---
+++ src/Layouts/ServiciosBoveda/CrearBoveda.jsx
@@ -0,0 +1,71 @@
+import { collection, addDoc } from "firebase/firestore";
+import { dataBase, subirImagen } from "../../config/dataBase";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const CrearBoveda = () => {
+    const [nombre,setNombre]=useState("");
+    const [urlWeb,setUrlWeb]=useState("")
+    const [contrasena,setContrasena]=useState("");
+    const [usuario,setUsuario]=useState("");
+    const [img, setImg] = useState(null);
+    
+    
+    const returnListado =useNavigate();
+
+    const agregarBoveda= async()=>{
+
+      const ulrImg = await subirImagen(img)
+        const servicioCollection = collection(dataBase,"Boveda");
+        const servicio = {
+          nombre,
+          urlWeb,
+          contrasena,
+          usuario,
+          ulrImg
+        };
+    
+        await addDoc(servicioCollection,servicio);
+        returnListado("/Boveda");
+         
+      };
+    
+    
+  return (
+    <div> <h1>Registrar Boveda</h1>
+    <section>
+     <form className="formBovedas">
+       <input
+         onChange={(e) => setNombre(e.target.value)}
+         placeholder={"Nombre"}
+         type={"text"}
+       />
+       <input
+         onChange={(e) => setUrlWeb(e.target.value)}
+         placeholder={"Url de Imagen"}
+         type={"text"}
+       />
+       <input
+         onChange={(e) => setContrasena(e.target.value)}
+         placeholder={"Contraseña"}
+         type={"text"}
+       />
+       <input
+         onChange={(e) => setUsuario(e.target.value)}
+         placeholder={"Usuario"}
+         type={"text"}
+       />
+        <section>Imagen:
+    <input onChange={(e)=>setImg(e.target.files[0])} type="file" /></section>
+        <input
+         onClick={agregarBoveda}
+         type={"button"}
+         value={"Agregar Bodeva"}
+       />
+       
+     </form>
+    </section></div>
+  )
+}
+
+export default CrearBoveda
--- src/Paginas/Proveedores.jsx
+++ src/Paginas/Proveedores.jsx
@@ -1,93 +1,24 @@
-import Menu from "../Componentes/Menu.jsx"
+
 import Footer from '../Componentes/Footer.jsx'
-import { collection, addDoc } from "firebase/firestore";
-import { dataBase,  } from "../config/dataBase.jsx";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import CrearProveedor from "../Layouts/ServicioProveedor/CrearProveedor.jsx"
+import ListarProveedor from "../Layouts/ServicioProveedor/ListarProveedor.jsx"
+import MenuAdmin from "../Componentes/MenuAdmin.jsx"
+
 const Proveedores = () => {
 
 
 
-  const [nombre, setNombre] = useState("");
-  const [direccion, setDireccion] = useState("");
-  const [ciudad, setCiudad] = useState("");
-  const [nit, setNit] = useState("");
-  const [telefono, setTelefono] = useState("");
-  const [nombreGerente, setNombreGerente] = useState("");
-  /*imagengerente*/
-  /*logoempresa */
   
-
-  const returnListado = useNavigate();
-  const agregarProveedor = async()=>{
-
-
-    const servicioCollection = collection(dataBase,"Proveedores");
-    const servicio={
-      nombre,
-      direccion,
-      ciudad,
-      nit,
-      telefono,
-      nombreGerente
-    };
-    await addDoc(servicioCollection,servicio);
-    returnListado("/proveedores")
-  };
   
  
 
 
   return (
     <div>
-      <Menu/>
-      <h1>Registrar Proveedores</h1>
-      
-      <section>
-        <form className="formProveedores">
-        <input 
-        /* Para los proveedores es necesario almacenar
-            nombre, dirección, ciudad, nit, teléfono 
-            gerente, nombre gerente, imagen gerente 
-            y el logo de la empresa*/
-
-          onChange={(e) => setNombre(e.target.value)}
-          placeholder={"Nombre "}
-          type={"text"}
-        />
-        <input
-        onChange={(e) => setDireccion(e.target.value)}
-        placeholder={"Direccion "}
-        type={"text"}
-        />
-        <input
-        onChange={(e) => setCiudad(e.target.value)}
-        placeholder={"Ciudad"}
-        type={"text"}
-        />
-        <input
-        onChange={(e) => setNit(e.target.value)}
-        placeholder={"Nit"}
-        type={"text"}
-        />
-        <input
-        onChange={(e) => setTelefono(e.target.value)}
-        placeholder={"Telefono"}
-        type={"text"}
-        />
-        <input
-        onChange={(e) => setNombreGerente(e.target.value)}
-        placeholder={"Nombre Gerente"}
-        type={"text"}
-        />
-        
-        <input
-          onClick={agregarProveedor}
-          type={"button"}
-          value={"Agregar proovedor"}
-        />
-        </form>
-      </section>
+      <MenuAdmin/>
+      <CrearProveedor/>
+      <h1>Listado de proveedores</h1>
+      <ListarProveedor/>
       <Footer/>
       </div>
   )

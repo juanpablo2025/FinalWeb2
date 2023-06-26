6b6f65171a15cd5c1819067496d1cc5b27b15cf1
yeris25 <123104349+yeris25@users.noreply.github.com>
2023-06-26 10:20:58 -0500

    Se actualizan los paginas con servicios

--- src/Paginas/Boveda.jsx
+++ src/Paginas/Boveda.jsx
@@ -1,40 +1,17 @@
-import Menu from "../Componentes/Menu.jsx"
+
+import CrearBoveda  from "../Layouts/ServiciosBoveda/CrearBoveda.jsx"
+import ListarBoveda from "../Layouts/ServiciosBoveda/ListarBoveda.jsx"
 import Footer from '../Componentes/Footer.jsx'
-import { collection, addDoc } from "firebase/firestore";
-import { dataBase,  } from "../config/dataBase.jsx";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import MenuAdmin from "../Componentes/MenuAdmin.jsx"
 
 
 
 
 const Boveda = () => {
 
-  const [nombre,setNombre]=useState("");
-  const [urlImagen,setUrlImagen]=useState("")
-  const [contrasena,setContrasena]=useState("");
-  const [usuario,setUsuario]=useState("");
-  /*imagen del sitio*/
+
+
   
-  const returnListado =useNavigate();
-
-  const agregarBoveda= async()=>{
-
-
-    const servicioCollection = collection(dataBase,"Bovedas");
-    const servicio = {
-      nombre,
-      urlImagen,
-      contrasena,
-      usuario
-    };
-
-    await addDoc(servicioCollection,servicio);
-    returnListado("/Bovedas");
-     
-  };
-
-
 
 
 
@@ -42,38 +19,10 @@ const Boveda = () => {
 
   return (
     <div>
-      <Menu/>
-     <h1>Registrar Boveda</h1>
-     <section>
-      <form className="formBovedas">
-        <input
-          onChange={(e) => setNombre(e.target.value)}
-          placeholder={"Nombre"}
-          type={"text"}
-        />
-        <input
-          onChange={(e) => setUrlImagen(e.target.value)}
-          placeholder={"Url de Imagen"}
-          type={"text"}
-        />
-        <input
-          onChange={(e) => setContrasena(e.target.value)}
-          placeholder={"Contraseña"}
-          type={"text"}
-        />
-        <input
-          onChange={(e) => setUsuario(e.target.value)}
-          placeholder={"Usuario"}
-          type={"text"}
-        />
-         <input
-          onClick={agregarBoveda}
-          type={"button"}
-          value={"Agregar Bodeva"}
-        />
-        
-      </form>
-     </section>
+      <MenuAdmin/>
+    <CrearBoveda/>
+    <h1>Listado de Contraseñas</h1>
+    <ListarBoveda/>
      <Footer/>
       </div>
   )
--- src/Paginas/Empleados.jsx
+++ src/Paginas/Empleados.jsx
@@ -1,102 +1,26 @@
-import Menu from "../Componentes/Menu.jsx"
-import Footer from '../Componentes/Footer.jsx'
 
-import { collection, addDoc } from "firebase/firestore";
-import { dataBase } from "../config/dataBase.jsx";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import Footer from '../Componentes/Footer.jsx'
+import CrearEmpleados from "../Layouts/ServiciosEmpleados/CrearEmpleados.jsx"
+
+import ListarEmpleados from "../Layouts/ServiciosEmpleados/ListarEmpleados.jsx"
+import MenuAdmin from "../Componentes/MenuAdmin.jsx"
 
 const Empleados = () => {
-  
-
-      const [nombre,setNombre]=useState("");
-      const [documento,setDocumento]=useState("");
-      const [correo,setCorreo]=useState("");
-      const [cargo,setCargo]=useState("");
-      const [salario,setSalario]=useState("");
-      const [direccion,setDireccion]=useState("");
-      const [cuenta,setCuenta]=useState("");
-     /*imagen del empleado*/ 
 
 
-     const returnListado = useNavigate();
-     const agregarEmpleado =async()=>{
 
-      const servicioCollection=collection(dataBase,"Empleados");
-      const servicio={
-        nombre,
-        documento,
-        correo,
-        cargo,
-        salario,
-        direccion,
-        cuenta
-      };
-      await addDoc(servicioCollection,servicio);
-      returnListado("/empleados")
-
-     }
 
 
 
 
 
   return (
-   
-    <div> <Menu/>
-    <h1>Registrar Empleados</h1>
-    <section>
-      <form className="formEmpleados">
-        <input
 
-        /*  Para los empleados es necesario guardar 
-      nombre, documento, correo, cargo, salario, 
-      dirección de residencia, número de cuenta bancaria,
-      imagen del empleado  
-  */
-        onChange={(e)=>setNombre(e.target.value)}
-        placeholder={"Nombre"}
-        type={"text"}
-        />
-        <input 
-        onChange={(e)=>setDocumento(e.target.value)}
-        placeholder={"Documento"}
-        type={"text"} 
-        />
-        <input
-        onChange={(e)=>setCorreo(e.target.value)}
-        placeholder={"Correo"}
-        type={"text"}
-        />
-        <input
-        onChange={(e)=>setCargo(e.target.value)}
-        placeholder={"Cargo"}
-        type={"text"}
-        />
-        <input
-        onChange={(e)=>setSalario(e.target.value)}
-        placeholder={"salario"}
-        type={"text"}
-        />
-        <input
-        onChange={(e)=>setDireccion(e.target.value)}
-        placeholder={"Direccion"}
-        type={"text"}
-        />
-        <input
-        onChange={(e)=>setCuenta(e.target.value)}
-        placeholder={"Cuenta"}
-        type={"text"}
-        />
-        
-        <input
-          onClick={agregarEmpleado}
-          type={"button"}
-          value={"Agregar empleados"}
-        />
-      </form>
-    </section> 
-    <Footer/></div>
+    <div> <MenuAdmin/>
+    <CrearEmpleados />
+    <h1>Listado de Empleado</h1>
+    <ListarEmpleados />
+    <Footer /></div>
   )
 }
 

84e58f7956442546998b64ef554ef85ac0a8f73c
alissoncaicedo <123104441+alissoncaicedo@users.noreply.github.com>
2023-06-29 10:06:39 -0500

    estilizar los formularios

--- src/Layouts/ServiciosClientes/CrearClientes.jsx
+++ src/Layouts/ServiciosClientes/CrearClientes.jsx
@@ -1,5 +1,5 @@
 import { collection, addDoc } from "firebase/firestore";
-import { dataBase,subirImagen } from "../../config/dataBase";
+import { dataBase, subirImagen } from "../../config/dataBase";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -13,13 +13,13 @@ const CrearClientes = () => {
   const [barrio, setBarrio] = useState("");
   const [ciudad, setCiudad] = useState("");
   const [img, setImg] = useState(null);
-  
+
   const returnListado = useNavigate();
   const agregarCliente = async () => {
     const ulrImg = await subirImagen(img)
 
-   
-    
+
+
     const servicioCollection = collection(dataBase, "Clientes");
     const servicio = {
       nombre,
@@ -30,74 +30,65 @@ const CrearClientes = () => {
       barrio,
       ciudad,
       ulrImg
-      
+
     };
     await addDoc(servicioCollection, servicio);
-    /*cambiar redireccion*/ 
+    /*cambiar redireccion*/
     returnListado("/Clientes");
   };
-  /*  <section className="form-register">
-    <h4>Formulario Registro</h4>
-    <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese su Nombre">
-    <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="Ingrese su Apellido">
-    <input class="controls" type="email" name="correo" id="correo" placeholder="Ingrese su Correo">
-    <input class="controls" type="password" name="correo" id="correo" placeholder="Ingrese su Contraseña">
-    <p>Estoy de acuerdo con <a href="#">Terminos y Condiciones</a></p>
-    <input class="botons" type="submit" value="Registrar">
-    <p><a href="#">¿Ya tengo Cuenta?</a></p>
-  </section>*/ 
+
   return (
     <div >
-    <section >
-    <form className="form-register">
+      <section >
+        <form className="form-register">
 
-      <section className="titulo-registrate">REGISTRATE</section>
-      <input className="controls"
-        onChange={(e) => setNombre(e.target.value)}
-        placeholder={"Nombre "}
-        type={"text"}
-      />
-      <input className="controls"
-        onChange={(e) => setDocumento(e.target.value)}
-        placeholder={"Documento "}
-        type={"text"}
-        max={'50'}
-      />
-      <input className="controls"
-        onChange={(e) => setCorreo(e.target.value)}
-        placeholder={"Correo"}
-        type={"text"}
-      />
-      <input className="controls"
-        onChange={(e) => setTelefono(e.target.value)}
-        placeholder={"Telefono"}
-        type={"text"}
-      />
-      <input className="controls"
-        onChange={(e) => setDireccion(e.target.value)}
-        placeholder={"Direccion"}
-        type={"text"}
-      />
-      <input className="controls"
-        onChange={(e) => setBarrio(e.target.value)}
-        placeholder={"Barrio"}
-        type={"text"}
-      />
-      <input className="controls"
-        onChange={(e) => setCiudad(e.target.value)}
-        placeholder={"Ciudad"}
-        type={"text"}
-      />
-       <section>Imagen:
-    <input className="botons" onChange={(e)=>setImg(e.target.files[0])} type="file" /></section>
-    
-      <input className="controls"
-        onClick={agregarCliente}
-        type={"button"}
-        value={"Agregar cliente"}
-      />
-    </form>
-  </section></div>
+          <section className="titulos-forms">REGISTRATE</section>
+          <input className="controls"
+            onChange={(e) => setNombre(e.target.value)}
+            placeholder={"Nombre "}
+            type={"text"}
+          />
+          <input className="controls"
+            onChange={(e) => setDocumento(e.target.value)}
+            placeholder={"Documento "}
+            type={"text"}
+            max={'50'}
+          />
+          <input className="controls"
+            onChange={(e) => setCorreo(e.target.value)}
+            placeholder={"Correo"}
+            type={"text"}
+          />
+          <input className="controls"
+            onChange={(e) => setTelefono(e.target.value)}
+            placeholder={"Telefono"}
+            type={"text"}
+          />
+          <input className="controls"
+            onChange={(e) => setDireccion(e.target.value)}
+            placeholder={"Direccion"}
+            type={"text"}
+          />
+          <input className="controls"
+            onChange={(e) => setBarrio(e.target.value)}
+            placeholder={"Barrio"}
+            type={"text"}
+          />
+          <input className="controls"
+            onChange={(e) => setCiudad(e.target.value)}
+            placeholder={"Ciudad"}
+            type={"text"}
+          />
+          <section>Imagen:
+            <input className="controls" onChange={(e) => setImg(e.target.files[0])} type="file" /></section>
+
+          <input className="botons"
+            onClick={agregarCliente}
+            type={"button"}
+            value={"Agregar cliente"}
+          />
+        </form>
+      </section></div>
   )
 }
 
--- src/Layouts/ServiciosEmpleados/CrearEmpleados.jsx
+++ src/Layouts/ServiciosEmpleados/CrearEmpleados.jsx
@@ -1,26 +1,26 @@
-import { collection,addDoc }  from "firebase/firestore";
-import { dataBase,subirImagen } from "../../config/dataBase";
+import { collection, addDoc } from "firebase/firestore";
+import { dataBase, subirImagen } from "../../config/dataBase";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const CrearEmpleados = () => {
 
-    const [nombre,setNombre]=useState("");
-    const [documento,setDocumento]=useState("");
-    const [correo,setCorreo]=useState("");
-    const [cargo,setCargo]=useState("");
-    const [salario,setSalario]=useState("");
-    const [direccion,setDireccion]=useState("");
-    const [cuenta,setCuenta]=useState("");
-   /*imagen del empleado*/ 
-   const [img, setImg] = useState(null);
+  const [nombre, setNombre] = useState("");
+  const [documento, setDocumento] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [salario, setSalario] = useState("");
+  const [direccion, setDireccion] = useState("");
+  const [cuenta, setCuenta] = useState("");
+  /*imagen del empleado*/
+  const [img, setImg] = useState(null);
 
-   const returnListado = useNavigate();
-   const agregarEmpleado =async()=>{
+  const returnListado = useNavigate();
+  const agregarEmpleado = async () => {
     const ulrImg = await subirImagen(img)
 
-    const servicioCollection=collection(dataBase,"Empleados");
-    const servicio={
+    const servicioCollection = collection(dataBase, "Empleados");
+    const servicio = {
       nombre,
       documento,
       correo,
@@ -29,69 +29,64 @@ const CrearEmpleados = () => {
       direccion,
       cuenta,
       ulrImg
-      
+
     };
-    await addDoc(servicioCollection,servicio);
+    await addDoc(servicioCollection, servicio);
     returnListado("/empleados")
 
-   }
+  }
   return (
     <div>
-    <h1>Registrar Empleados</h1>
-    <section>
-      <form className="formEmpleados">
-        <input
+      <section>
+        <form className="form-register">
+          <section className="titulos-forms">REGISTRAR EMPLEADOS</section>
+          <input className="controls"
 
-        /*  Para los empleados es necesario guardar 
-      nombre, documento, correo, cargo, salario, 
-      dirección de residencia, número de cuenta bancaria,
-      imagen del empleado  
-  */
-        onChange={(e)=>setNombre(e.target.value)}
-        placeholder={"Nombre"}
-        type={"text"}
-        />
-        <input 
-        onChange={(e)=>setDocumento(e.target.value)}
-        placeholder={"Documento"}
-        type={"text"} 
-        />
-        <input
-        onChange={(e)=>setCorreo(e.target.value)}
-        placeholder={"Correo"}
-        type={"text"}
-        />
-        <input
-        onChange={(e)=>setCargo(e.target.value)}
-        placeholder={"Cargo"}
-        type={"text"}
-        />
-        <input
-        onChange={(e)=>setSalario(e.target.value)}
-        placeholder={"salario"}
-        type={"text"}
-        />
-        <input
-        onChange={(e)=>setDireccion(e.target.value)}
-        placeholder={"Direccion"}
-        type={"text"}
-        />
-        <input
-        onChange={(e)=>setCuenta(e.target.value)}
-        placeholder={"Cuenta"}
-        type={"text"}
-        />
+            onChange={(e) => setNombre(e.target.value)}
+            placeholder={"Nombre"}
+            type={"text"}
+          />
+          <input className="controls"
+            onChange={(e) => setDocumento(e.target.value)}
+            placeholder={"Documento"}
+            type={"text"}
+          />
+          <input className="controls"
+            onChange={(e) => setCorreo(e.target.value)}
+            placeholder={"Correo"}
+            type={"text"}
+          />
+          <input className="controls"
+            onChange={(e) => setCargo(e.target.value)}
+            placeholder={"Cargo"}
+            type={"text"}
+          />
+          <input className="controls"
+            onChange={(e) => setSalario(e.target.value)}
+            placeholder={"salario"}
+            type={"text"}
+          />
+          <input className="controls"
+            onChange={(e) => setDireccion(e.target.value)}
+            placeholder={"Direccion"}
+            type={"text"}
+          />
+          <input className="controls"
+            onChange={(e) => setCuenta(e.target.value)}
+            placeholder={"Cuenta"}
+            type={"text"}
+          />
 
-<section>Imagen:
-    <input onChange={(e)=>setImg(e.target.files[0])} type="file" /></section>
-        
-        <input
-          onClick={agregarEmpleado}
-          type={"button"}
-          value={"Agregar empleados"}
-        />
-      </form>
-    </section> 
+          <section>Imagen:
+            <input className="controls" onChange={(e) => setImg(e.target.files[0])} type="file" /></section>
+
+          <input className="botons"
+            onClick={agregarEmpleado}
+            type={"button"}
+            value={"Agregar empleados"}
+          />
+        </form>
+      </section>
     </div>
   )
 }
